80726f9dca25aa09fa2cba6a6a5c6d0f9b71b429
Bjoern Mayer <u301101@mlogin101.hpc.dkrz.de>
2022-02-07 17:52:29 +0100

    Added script to start SlurmCluster

---
+++ src/dask_cluster.py
@@ -0,0 +1,46 @@
+import prefect
+from prefect import task, Flow
+import xarray as xr
+
+import xarray as xr
+import pathlib
+import os
+import numpy as np
+import argparse
+from prefect.engine.executors import DaskExecutor
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cluster")
+args = parser.parse_args()
+
+cluster_address = args.cluster
+print(cluster_address)
+
+from dask.distributed import Client
+
+
+
+proj_dir = pathlib.Path.cwd().resolve().parents[0]
+data_dir = os.path.join(proj_dir, "data")
+data_raw_dir = os.path.join(data_dir, "raw")
+data_pro_dir = os.path.join(data_dir, "processed")
+
+if not os.path.exists(data_pro_dir):
+    os.mkdir(data_pro_dir)
+
+
+
+logger = prefect.context.get("logger")
+
+
+@task
+def create_data(size):
+    data = xr.DataArray(np.random.normal(size=size))
+    data.to_netcdf("test.nc")
+
+
+dask_executor = DaskExecutor(address = cluster_address)
+with Flow("mistral_test", executor = dask_executor) as flow:
+    create_data((10**3,10**3,10**3))
+
+flow.register(project_name = "Test")
---
+++ src/start_slurmcluster.py
@@ -0,0 +1,37 @@
+import logging
+from dask_jobqueue import SLURMCluster
+from dask.distributed import Client
+import argparse
+
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--name", type=str, help = "Name of the job which appears in Slurm", default ="dask-cluster")
+parser.add_argument("--partition", type=str, help="Name of the partition where the job will run", default = "shared", choices = ["shared", "prepost","compute","compute2"])
+parser.add_argument("--project", type=str, help = "Name of the project")
+parser.add_argument("--cores", type=int, help="Number of cores")
+parser.add_argument("--interface", type=str, default = "ib0")
+parser.add_argument("--memory", type = str, help = "Size of memory reserved")
+parser.add_argument("--workers", type= int, help = "Number of workers")
+parser.add_argument("--port", type=int, default = 8080, help = "Portnumber for dashboard") 
+
+args = parser.parse_args()
+
+name = args.name
+queue = args.partition
+project = args.project
+cores = args.cores
+interface = args.interface
+memory = args.memory
+workers = args.workers
+port=args.port
+
+cluster = SLURMCluster(name=name, queue = queue, project= project, cores=cores, interface=interface, memory=memory, scheduler_options={"dashboard_address": ":{}".format(str(port))})
+client = Client(cluster)
+
+cluster.scale(workers)
+
+logging.info(client)
+
+input("Press enter to close cluster")
